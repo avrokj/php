dcb109bb75b40341a377082bf83187f897db0acb
avrokj <jan.korva@gmail.com>
2023-09-04 14:35:34 +0300

    fix

--- delete.php
+++ delete.php
@@ -6,6 +6,5 @@ $id = $_POST['id'];
 
 $stmt = $pdo->prepare('UPDATE books SET is_deleted=1 WHERE id = :id'); // statement objekti
 $stmt->execute(['id' => $id]); // tagastab statementi
-$book = $stmt->fetch();
 
 header('Location: index.php');
