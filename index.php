2143ccaba3f0d3ffa3b80dbb1e1fbb7fa7ef8d72
avrokj <jan.korva@gmail.com>
2023-09-10 16:04:05 +0300

    h1 and count

--- index.php
+++ index.php
@@ -4,16 +4,31 @@ require_once('./connection.php');
 
 $stmt = $pdo->query('SELECT * FROM books where is_deleted <> 1');
 
-echo "<ul>";
+?>
 
-while ($row = $stmt->fetch())
-{
-    ?> 
+<h1>Raamatud</h1>
+
+<?php
+$count = $stmt->rowCount();
+echo "<p>Meil on <strong>$count</strong> raamatut.</p>";
+?>
+
+<form action="./search.php" method="post">
+    <input type="text" name="SearchValue" id="" placeholder="Sisesta raamatu nimi...">
+    <button name="search" title="Otsi">Otsi</button>
+</form>
+<ul>
+
+    <?php
+    while ($row = $stmt->fetch()) {
+    ?>
         <li>
             <a href="./book.php?id=<?= $row['id']; ?>">
-                <?= $row['title'];?>
+                <?= $row['title']; ?>
             </a>
+            <small><?= $row['id']; ?></small>
         </li>
-<?php
-}?>
-echo "</ul>";
+    <?php
+    }
+    ?>
+</ul>
