17f81f5322f06c3ac57a38154e1cddbcdf9d5b00
avrokj <jan.korva@gmail.com>
2023-09-20 23:32:39 +0300

    add book link

--- src/footer.php
+++ src/footer.php
@@ -1,7 +1,7 @@
 <footer class="flex bg-gray-200 dark:bg-slate-800 shadow rounded-b-lg grid grid-cols-2 p-4">
   <div>&copy; Raamatud <?= date("Y"); ?></div>
   <div class="text-right">
-    <a href="./" class="px-4"><i class="fa fa-book"></i> Lisa raamat </a>
+    <a href="./add_book.php" class="px-4"><i class="fa fa-book"></i> Lisa raamat </a>
     <a href="./add_author.php" class="px-4"><i class="fa fa-user-plus"></i> Lisa autor </a>
   </div>
 </footer>
